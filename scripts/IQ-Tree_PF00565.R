e3606564d3af6a1141a8d1ba0994cccfd2b89794
Hailey Louw <hlouw@wisc.edu>
2024-03-11 13:14:49 -0500

    added files and IQ-Tree Scripts for PF00067 and PF00041

--- scripts/IQ-Tree_PF00565.R
+++ scripts/IQ-Tree_PF00565.R
@@ -8,6 +8,6 @@
 
 `cd hlouw@solislemus-001.discovery.wisc.edu/mnt/ws/home/hlouw/iqtree-project`
 
-# IQ-Tree command
+# IQ-Tree command with 1000 Ultrafast Bootstrap replicates
 
-`./iqtree-1.6.12-Linux/bin/iqtree -s /mnt/ws/home/hlouw/iqtree-project/PF00565_full-seq_msa_char.fasta -m MFP -m LG+G20 -b 100`
+`./iqtree-1.6.12-Linux/bin/iqtree -s /mnt/ws/home/hlouw/iqtree-project/PF00565_full-seq_msa_char.fasta -m MFP -m LG+G20 -bb 1000`
