ce4062e4ef6cfe06db662bdd3a925d592257c020
Evan Gorstein <egorstein@wisc.edu>
2024-07-02 18:35:06 -0400

    restore accidentally deleted gen_seq_lg.sh file

---
+++ data/simulations/gen_seq_lg.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Check that a tree file was provided as an argument
+if [ "$#" -ne 1 ]; then
+    echo "Usage: $0 <tree_file>"
+    exit 1
+fi
+# Wrangle file paths and create output path name
+fam_name=$(basename "$1" .tree_cleaned)
+n_seq=$(basename "$(dirname "$1")")
+output_dir="msas/${n_seq}"
+output_file="${output_dir}/${fam_name}_msa.dat"
+# Create output directory if it doesn't exist
+if [ ! -d "$output_dir" ]; then
+  mkdir -p "$output_dir"
+fi
+
+# Read in the exchange parameters and equilibrium frequencies from the output of the Python script
+read -r exchanges freqs < <(python get_lg_params.py)
+exchanges=${exchanges//,/ } # Replace commas separating the exchange parameters with spaces, as this is required for seq-gen to read it as a command line argument for some reason
+#echo -e "Exchange parameters:\n$exchanges"
+#echo -e "Equilibrium frequencies:\n$freqs"
+# Run seq-gen with the specified parameters and the tree file
+seq-gen  -mGENERAL -z770 -l100 -wa -on -f $freqs -r $exchanges  < "$1" > "$output_file"
