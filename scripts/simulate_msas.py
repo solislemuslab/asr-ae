e50fb2e1707ae3aef493a020c803975c1f9e52a3
Evan Gorstein <egorstein@wisc.edu>
2025-04-14 18:15:18 -0500

    compute scale

--- scripts/simulate_msas.py
+++ scripts/simulate_msas.py
@@ -181,6 +181,8 @@ if __name__ == "__main__":
         states = constants.AA
     assert np.allclose(Q @ np.ones_like(pi), 0, atol=1e-6)
     assert np.allclose(pi @ Q, 0, atol=1e-6)
+    print(compute_scale(Q, pi))
+    sys.exit(0)
     
     # set seed
     random.seed(770)
